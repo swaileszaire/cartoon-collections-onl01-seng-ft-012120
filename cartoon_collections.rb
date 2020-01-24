5d90a6faed3cb239880700e4aad3c9c48b641b5f
Zaire  Swailes <swaileszaire@gmail.com>
2020-01-24 03:13:54 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,19 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarves)
+dwarves = ["Doc", "Dopey", "Bashful", "Grumpy"]
+dwarves.map do 
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(planeteer_calls)
+planeteer_calls = ["earth","wind", "fire", "water", "heart"].capitalize.first.letter do |planeteer_calls| << "!"
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls?(calls)
+  calls = "puff", "Bowie", "controller", "grass"
+  calls.find do 
+    if |calls| <= 4 then = true 
+      else |calls| >= 4 then = false
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
-  cheese_types = ["cheddar", "gouda", "camembert"]
+def find_the_cheese(cheese)
+  cheese_types = ["cheddar", "gouda", "camembert"].include do |cheese|
 end
