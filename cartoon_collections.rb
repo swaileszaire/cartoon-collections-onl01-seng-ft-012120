dec20739c6177b29073bfc3aa73876ceb9405b0e
Zaire  Swailes <swaileszaire@gmail.com>
2020-01-24 03:06:54 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -15,5 +15,5 @@ def long_planeteer_calls?(calls)
 end
 
 def find_the_cheese(cheese)
-  cheese_types = ["cheddar", "gouda", "camembert"]
-  
+  cheese_types = ["cheddar", "gouda", "camembert"].include do |cheese|
+    if |cheese|
